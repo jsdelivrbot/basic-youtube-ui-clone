1af1097f67865b5a381976f56a405ccb7f38df21
fattymelt <seth.m.green@gmail.com>
2017-06-21 12:57:06 -0400

    Adding Youtube API and API Key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar.js'
+import Config from './config.js';
+
+// You'll need to get your own API key and put it in a config.js file ...
+// config.js:
+//
+//  export default {
+//    API_KEY: "AIzaSyAkS8n1Rhrv6GggOBaAr7eNr2A7wJhTDvg"
+//  }
+//
+
+const API_KEY = Config.API_KEY;
 
 // Create a new component. This compoenet should produce some HTML
 
 const App = () => {
-  return <div>Hello World!</div>
+  return <div>Hello World <SearchBar /></div>
 }
 
 // Take this component's generated HTML and put it on the page
